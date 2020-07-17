f27cd59ae4449f17da33943b6d26517ae133e500
Stephan Rajaratnam <stephanrajaratnam@gmail.com>
2020-07-17 07:03:55 -0400

    Redux setup in project (#4)
    Stephan Rajaratnam

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
 import { App } from 'components'
 import { unregister } from 'core'
 
+import store from 'ducks'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
